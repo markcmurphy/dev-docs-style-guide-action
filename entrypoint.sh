8a756b80659dfd9e50cfaa53a686bfd712e23ed6
Mark Murphy <mark@murphymark.me>
2021-03-25 11:27:40 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,17 +6,5 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 echo "Some console message 04" 
  
-quality-docs ./test.md 2>&1 >/dev/null \
-  | reviewdog \
-      -efm='%-P%f' \
-      -efm=' %#%l:%c-%[0-9]%#:%[0-9]%# %# %trror  %m' \
-      -efm=' %#%l:%c-%[0-9]%#:%[0-9]%# %# %tarning  %m' \
-      -efm='%-Q' \
-      -efm='%-G%.%#' \
-      -name="quality-docs" \
-      -reporter="${INPUT_REPORTER:-github-pr-check}" \
-      -filter-mode="${INPUT_FILTER_MODE}" \
-      -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-      -level="${INPUT_LEVEL}" \
-      ${INPUT_REVIEWDOG_FLAGS}
+quality-docs ./test.md
       
