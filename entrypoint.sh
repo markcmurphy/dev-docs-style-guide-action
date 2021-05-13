185555bd1628ef971d4fbd48af0657f5be99d57c
Mark Murphy <mark.murphy@bigcommerce.com>
2021-05-13 13:53:47 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-find . -type f -exec echo "quality-docs *.md" \; \
+find . -type f -exec quality-docs *.md \; \
   | reviewdog \
       -efm='%-P%f' \
       -efm=' %#%l:%c-%[0-9]%#:%[0-9]%# %# %trror  %m' \
